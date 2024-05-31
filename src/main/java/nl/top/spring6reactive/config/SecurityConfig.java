b72277c33b8efb3f0086a58b7bd8b6ab0c7095f3
Liliane Top <hallo@lilianetop.nl>
2024-05-31 16:19:24 +0200

    added spring boot actuator including config and properties

--- src/main/java/nl/top/spring6reactive/config/SecurityConfig.java
+++ src/main/java/nl/top/spring6reactive/config/SecurityConfig.java
@@ -1,7 +1,9 @@
 package nl.top.spring6reactive.config;
 
+import org.springframework.boot.actuate.autoconfigure.security.reactive.EndpointRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
@@ -10,10 +12,20 @@ import org.springframework.security.web.server.SecurityWebFilterChain;
 @Configuration
 @EnableWebFluxSecurity
 public class SecurityConfig {
+
     @Bean
+    @Order(1)
+    SecurityWebFilterChain actuatorSecurityFilterChain(ServerHttpSecurity http) throws Exception {
+        http.securityMatcher(EndpointRequest.toAnyEndpoint())
+                .authorizeExchange(authorize -> authorize.anyExchange().permitAll());
+
+        return http.build();
+    }
+    @Bean
+    @Order(2)
     SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
         http
-                .csrf((csrfSpec -> csrfSpec.disable()))
+                .csrf((ServerHttpSecurity.CsrfSpec::disable))
                 .authorizeExchange(authorize -> authorize.anyExchange().authenticated())
                 .oauth2ResourceServer(resourceServer -> resourceServer.jwt(Customizer.withDefaults()));
         return http.build();
